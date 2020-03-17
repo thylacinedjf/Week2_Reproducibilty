f3625f58b9f47e72dbfead3d76bd0bc0ddc4c2d2
derek Fulton <derek.fulton@csiro.au>
2020-03-17 22:40:19 +1100

    Maybe I got it right? But it's bed time. Nighty. Night

--- bom_data.R
+++ bom_data.R
@@ -22,18 +22,40 @@ group_by(Month) %>%
 summarise(temp_mean = mean(Temp_diff, na.rm = TRUE)) %>% 
 arrange(temp_mean)
 
-# Question 3 Does the westmost (lowest longitude) or eastmost (highest longitude)
-# weather station in our dataset have a higher average solar exposure?
+# Question 3 Which state saw the lowest average daily temperature difference
+
+# STEP1 gather and spread data
 
 bom_station_data <- bom_stations %>% 
   gather(Station_ID, Misc, -info) %>% 
-  spread(info, Misc) %>% 
-  mutate(as.numeric(Station_ID))
+  spread(info, Misc) %>%
+  mutate(Station_ID = as.integer(gsub("\\X","",bom_station_data$Station_ID)))
 
-bom_station_rename <- rename(bom_station_data, Station_number = as.numeric(Station_number))
+# STEP2 convert temp min and max from chr to numbers
 
-bom_station_data <- mutate(bom_station_data, station_number = as.character(Station_ID))
+temp_min_max_sep <- mutate(temp_min_max_sep, temp_min = as.numeric(temp_min))
+temp_min_max_sep <- mutate(temp_min_max_sep, temp_max = as.numeric(temp_max))
 
- 
+# STEP3 join the two data sets
+
+names(bom_station_data)[names(bom_station_data) == "Station_ID"] <- "Station_number"
+combined_bom_data <- full_join(temp_min_max_sep, bom_station_data, 'Station_number')
+
+# STEP4 sort by state and arrange
+
+lowest_average_temp <- group_by(combined_bom_data, state) %>%
+  summarise(lowest_average_temp = mean(temp_min, na.rm = TRUE)) %>%
+  arrange(lowest_average_temp)
+View(lowest_average_temp)
   
-full_join(bom_station_rename, bom_station_data)
+# Question 4 Does the westmost (lowest longitude) or eastmost (highest longitude)
+# weather station in our dataset have a higher average solar exposure?
+
+# STEP WHATEVER! group by station number and longitude.
+# get the mean of Solar_exposure
+# arrange by longitude. Save. Commit. Push. Have a stiff drink and go to bed!
+
+solar_exp <- group_by(combined_bom_data, Station_number, lon) %>%
+  summarise(solar_exp = mean(as.numeric(as.character(Solar_exposure)), na.rm = TRUE)) %>%
+  arrange(lon)
+View(solar_exp)
